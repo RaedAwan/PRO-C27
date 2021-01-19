537226904effead143042f49fb98ce8f110f6fd4
RaedAwan <75720005+RaedAwan@users.noreply.github.com>
2021-01-19 12:41:55 +0300

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,75 +1,140 @@
+
 const Engine = Matter.Engine;
-const World= Matter.World;
+const World = Matter.World;
 const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+const Render = Matter.Render;
 const Constraint = Matter.Constraint;
-var engine, world;
-var box1, pig1;
-var backgroundImg,platform;
+var bobObject1,bobObject2,bobObject3, bobObject4,bobObject5, roofObject
+var rope1,rope2,rope3, rope4,rope5;
+var world;
 
-function preload() {
-    backgroundImg = loadImage("sprites/bg.png");
+
+
+function setup() {
+	createCanvas(800, 700);
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	roofObject=new Roof(width/2,height/4,width/7,20);
+
+	bobDiameter=40;
+
+	startBobPositionX=width/2;
+	startBobPositionY=height/4+500;
+	bobObject1=new Bob(startBobPositionX-bobDiameter*2,startBobPositionY,bobDiameter);
+	bobObject2=new Bob(startBobPositionX-bobDiameter,startBobPositionY,bobDiameter);
+	bobObject3=new Bob(startBobPositionX,startBobPositionY,bobDiameter);
+	bobObject4=new Bob(startBobPositionX+bobDiameter,startBobPositionY,bobDiameter);
+	bobObject5=new Bob(startBobPositionX+bobDiameter*2,startBobPositionY,bobDiameter);
+
+	var render = Render.create({
+		element: document.body,
+		engine: engine,
+		options: {
+		  width: 1200,
+		  height: 700,
+		  wireframes: false
+		}
+	  });
+
+	  rope1=new Rope(bobObject1.body,roofObject.body,-bobDiameter*2, 0)
+
+	rope2=new Rope(bobObject2.body,roofObject.body,-bobDiameter*1, 0)
+	rope3=new Rope(bobObject3.body,roofObject.body,0, 0)
+	rope4=new Rope(bobObject4.body,roofObject.body,bobDiameter*1, 0)
+	rope5=new Rope(bobObject5.body,roofObject.body,bobDiameter*2, 0)
+
+	/*constraint1={
+		bodyA:bobObject1.body,
+		bodyB:roofObject.body,
+		pointB: {x:-bobDiameter*2, y:0}
+	}
+	constraint2={
+		bodyA:bobObject2.body,
+		bodyB:roofObject.body,		
+		pointB: {x:-bobDiameter, y:0}
+	}
+	constraint3={
+		bodyA:bobObject3.body,
+		bodyB:roofObject.body,		
+		pointB: {x:0, y:0}
+	}
+	constraint4={
+		bodyA:bobObject4.body,
+		bodyB:roofObject.body,		
+		pointB: {x:bobDiameter, y:0}	
+	}
+	constraint5={
+		bodyA:bobObject5.body,
+		bodyB:roofObject.body,		
+		pointB: {x:bobDiameter*2, y:0}
+	}
+	var pendulum1=Constraint.create(constraint1)
+	var pendulum2=Constraint.create(constraint2)
+	var pendulum3=Constraint.create(constraint3)
+	var pendulum4=Constraint.create(constraint4)
+	var pendulum5=Constraint.create(constraint5)
+	World.add(world, pendulum1);
+	World.add(world, pendulum2);
+	World.add(world, pendulum3);
+	World.add(world, pendulum4);
+	World.add(world, pendulum5);
+	*/
+	Engine.run(engine);
+	//Render.run(render);
+  
 }
 
-function setup(){
-    var canvas = createCanvas(1200,400);
-    engine = Engine.create();
-    world = engine.world;
+	//Create the Bodies Here.
 
 
-    ground = new Ground(600,height,1200,20);
-    platform = new Ground(150, 305, 300, 170);
+	Engine.run(Engine);
+  
 
-    box1 = new Box(700,320,70,70);
-    box2 = new Box(920,320,70,70);
-    pig1 = new Pig(810, 350);
-    log1 = new Log(810,260,300, PI/2);
 
-    box3 = new Box(700,240,70,70);
-    box4 = new Box(920,240,70,70);
-    pig3 = new Pig(810, 220);
 
-    log3 =  new Log(810,180,300, PI/2);
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  roofObject.display();
 
-    box5 = new Box(810,160,70,70);
-    log4 = new Log(760,120,150, PI/7);
-    log5 = new Log(870,120,150, -PI/7);
+  rope1.display()
+  rope2.display()
+  rope3.display()
+  rope4.display()
+  rope5.display()	
+  bobObject1.display();
+  bobObject2.display();
+  bobObject3.display();
+  bobObject4.display();
+  bobObject5.display();
+  
+ 
+}
+function keyPressed() {
+	if (keyCode === UP_ARROW) {
 
-    bird = new Bird(100,100);
-   //log6 = new Log(230,180, 80, PI/2);
-   slingshot = new Slingshot(bird.body, {x: 200, y: 100});
+	  Matter.Body.applyForce(bobObject1.body,bobObject1.body.position,{x:-50,y:-45});
 
-   
+	}
 }
 
-function draw(){
-    background(backgroundImg);
-    Engine.update(engine);
-    console.log(box2.body.position.x);
-    console.log(box2.body.position.y);
-    console.log(box2.body.angle);
-    box1.display();
-    box2.display();
-    ground.display();
-    pig1.display();
-    log1.display();
 
-    box3.display();
-    box4.display();
-    pig3.display();
-    log3.display();
+function drawLine(constraint)
+{
+  bobBodyPosition=constraint.bodyA.position
+  roofBodyPosition=constraint.bodyB.position
 
-    box5.display();
-    log4.display();
-    log5.display();
+  roofBodyOffset=constraint.pointB;
+  
+  roofBodyX=roofBodyPosition.x+roofBodyOffset.x
+  roofBodyY=roofBodyPosition.y+roofBodyOffset.y
+  line(bobBodyPosition.x, bobBodyPosition.y, roofBodyX,roofBodyY);
+}
 
-    bird.display();
-    platform.display();
-    //log6.display();
-    slingshot.display();
-}
-function mouseDragged(){
-Matter.Body.setPosition(bird.body, {x: mouseX, y: mouseY});
-}
-function mouseReleased(){
-    slingshot.fly();
-}
+
+
+
